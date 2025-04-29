33d045d564f078c2284246f21bc700b878f96daf
ekeledochidi <ekeledochidiebere@gmail.com>
2025-04-29 22:06:45 +0100

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import './App.css';
 import ProductList from './ProductList';
 import axios from 'axios';
 
+
 function App() {
   const navigate = useNavigate();
 
@@ -35,13 +36,45 @@ function App() {
       <div class="sticky">
        <ul class="horizontal">
           <img src="Direction.jpg" className="logoo" alt="web logo" />
-          <h4 style={{float: 'left', color: 'black', padding: '6px'}}>Sw</h4>
-          <li style={{float: 'right'}}><a href="mailto:ekeledochidiebere@gmail.comil"><i class="fa-solid fa-envelope" style={{fontSize: '22px', paddingTop: '5px'}}></i></a></li>
+          <h5 style={{float: 'left', color: 'black', padding: '10px 5px'}}>Sw <span style={{fontSize: '12px'}}>-more than just a blog.</span></h5>
+          <li style={{float: 'right', color: 'black'}}>ENG<a data-bs-toggle="modal" data-bs-target="#myModal"><i class="fa-solid fa-envelope" style={{fontSize: '22px', paddingTop: '5px'}}></i></a></li>
        </ul>
       </div>
+      
       <video autoPlay loop muted playsInline className="video-background">
         <source src="videobg.mp4" type="video/mp4" />
       </video>
+
+      
+      <div class="modal fade" id="myModal">
+       <div class="modal-dialog">
+           <div class="modal-content">
+    
+               
+               <div class="modal-header">
+                   <p>Contact info</p>
+                   <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+               </div>
+
+               
+               <div class="modal-body">
+                   <i class="fa-solid fa-mobile"></i>
+                   <li>+234-8184207769</li>
+                   <li>Mobile Number</li>
+                   <i class="fa-solid fa-envelope"></i>
+                   <li>ekeledochidiebere@gmail.com</li>
+                   <li>E-mail</li>
+               </div>
+
+               
+               <div class="modal-footer">
+                   <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
+               </div>
+
+           </div>
+       </div>
+      </div>
+
       <div className="container-fluid">
         <div className="welcome col-xxl-6 col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12">
           <h3>Welcome to our website!</h3>
@@ -69,6 +102,11 @@ function App() {
         <div className="col-xxl-6 col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12" style={{ float: 'left', padding: '50px' }}>
           <img src="AI.jpg" className="logo" alt="AI logo" />
         </div>
+      </div>
+
+      <div className='footer-container'>
+        <p style={{fontWeight: 'bold'}}>Powered by CHIDI</p>
+        <p style={{fontStyle: 'italic'}}>-All rights reserved</p>
       </div>
     </div>
   );
---
+++ src/Footer.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+
+function Footer() {
+    return(
+        <div>
+            <div>
+                <p>Sideways -yes! Sw.</p>
+                <p>All rights reserved</p>
+                <p>2025</p>
+                <p class="popular">Follow developer</p>
+                <ul class="follow">
+                    <li><a href="https://www.facebook.com/ekeledo.chidiebere"><i class="fab fa-facebook"></i></a></li>
+                    <li><a href="https://instagram.com/ekeledo_chidi?igshid=ZDc4ODBmNjlmNQ=="><i class="fab fa-instagram"></i></a></li>
+                    <li><a href="https://www.snapchat.com/add/ekeledochidi?share_id=UrOQw6KGail&locale=en-GB"><i class="fab fa-snapchat"></i></a></li>
+                    <li><a href="https://twitter.com/EkeledoChidi?t=_ywd45VsrsRoyJUYVSCD2Q&s=09"><i class="fab fa-twitter"></i></a></li>
+                </ul>
+            </div>
+
+            <div>Expore</div>
+
+            <div>
+                <p>For Users</p>
+                <ul>
+                    <li>Documentation</li>
+                    <li>API</li>
+                    <li>Review</li>
+                </ul>
+            </div>
+
+            <div>
+                <p>Company</p>
+                <ul>
+                    <li>About</li>
+                    <li>Contact</li>
+                    <li>Donations</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default Footer;
